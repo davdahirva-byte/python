4855b5e0814f62e40d0dd1f9cdf60e11e09b5cb4
davdahirva-byte <davdahirva@gmail.com>
2026-04-29 15:23:15 +0530

    Update and rename p21kgtogram.py to p21kg_to_gram.py

--- p21kg_to_gram.py
+++ p21kg_to_gram.py
@@ -3,9 +3,6 @@
 # Ask the user to enter weight of a package (kg).
 # Convert it into grams and display the result.
 
-# Hint:
-# 👉 Remember the kg → grams conversion.
-
 weight=float(input("enter weight of package"))
 
 grams=weight*1000
